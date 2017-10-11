62936788eaf9020163faa1710989f9480587cb55
Carlos Salgado <csalgado307@gmail.com>
2017-10-11 15:30:41 -0700

    indent

--- js/script.js
+++ js/script.js
@@ -5,40 +5,40 @@ document.getElementById('loadQuote').addEventListener("click", printQuote, false
  // array of quote objects
 var quotes = [
 {
-quote: "The only source of knowledge is experience.",
-source: "Albert Einstein",
-citation: "German Theoretical-Physicist",
-year: 1879
+  quote: "The only source of knowledge is experience.",
+  source: "Albert Einstein",
+  citation: "German Theoretical-Physicist",
+  year: 1879
 },
 {
-quote: "Create the future of education so I can change as many lives as possible.",
-source: "Ryan Carson",
-citation: "twitter.com",
-year: 2017
+  quote: "Create the future of education so I can change as many lives as possible.",
+  source: "Ryan Carson",
+  citation: "twitter.com",
+  year: 2017
 },
 {
-quote: "If you don't find a way to make money while you sleep, you will work until you die.",
-source: "Warren Buffett",
-citation: "azquotes.com",
-year:
+  quote: "If you don't find a way to make money while you sleep, you will work until you die.",
+  source: "Warren Buffett",
+  citation: "azquotes.com",
+  year:
 },
 {
-quote: "I choose a lazy person to do a hard job. Because a lazy person will find an easy way to do it.",
-source: "Bill Gates",
-citation: "geckoandfly.com",
-year:
+  quote: "I choose a lazy person to do a hard job. Because a lazy person will find an easy way to do it.",
+  source: "Bill Gates",
+  citation: "geckoandfly.com",
+  year:
 },
 {
-quote: "Maybe they hate me because I'm too good.",
-source: "Cristiano Ronaldo",
-citation: "successsStory.com",
-year:
+  quote: "Maybe they hate me because I'm too good.",
+  source: "Cristiano Ronaldo",
+  citation: "successsStory.com",
+  year:
 },
 {
-quote: "I'm not talented, I'm obsessed.",
-source: "Conor McGregor",
-citation: "twitter.com",
-year:
+  quote: "I'm not talented, I'm obsessed.",
+  source: "Conor McGregor",
+  citation: "twitter.com",
+  year:
 }
 ];
 
